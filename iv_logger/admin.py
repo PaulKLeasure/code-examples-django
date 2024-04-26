eab44875c6afaaaff5c025bad4f90b4bf022cf32
Paul Leasure <paul@techaccelerator.com>
2024-04-26 12:46:36 -0400

    ..housecleaning ...

--- iv_logger/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import IvaultLog
-
-admin.site.register(IvaultLog)
--- iv_logger/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IvLoggerConfig(AppConfig):
-    name = 'iv_logger'
